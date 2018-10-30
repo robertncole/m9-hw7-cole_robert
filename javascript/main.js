4d12b9654e6825297dbcea0751847fbc648685fe
Robert Cole <robertcole@Roberts-MBP.lan1>
2018-10-30 01:13:48 -0400

    Add hover opacity to boxes

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,27 @@
 var button = document.getElementsByTagName('button');
-
-console.log (button);
+var boxesFaded = document.getElementsByClassName('box-js');
 
 var locationDiv = document.getElementsByClassName('hide-div');
 
 function changeColor() {
   this.classList.toggle('change_color');
-}
+};
+
+function opacity() {
+  this.classList.toggle('box');
+};
 
 function showDiv() {
   this.nextElementSibling.classList.toggle('show-div');
-}
+};
 
 for (i=0; i < button.length; i++) {
   button[i].addEventListener('mouseenter', changeColor);
   button[i].addEventListener('mouseleave', changeColor);   button[2].addEventListener('click', showDiv); button[6].addEventListener('click', showDiv);
-}
+};
+
+for (i=0; i < boxesFaded.length; i++) {
+  boxesFaded[i].addEventListener('mouseenter', opacity);
+  boxesFaded[i].addEventListener('mouseleave', opacity);
+};
+
