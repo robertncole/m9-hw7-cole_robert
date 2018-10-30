a5b0ba875591c26f4651c385681a1cbb5c0d0430
Robert Cole <robertcole@Roberts-MBP.lan1>
2018-10-30 00:44:31 -0400

    Add locations drop down

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,18 @@
 var button = document.getElementsByTagName('button');
 
+console.log (button);
+
+var locationDiv = document.getElementsByClassName('hide-div');
+
 function changeColor() {
   this.classList.toggle('change_color');
-};
+}
 
+function showDiv() {
+  this.nextElementSibling.classList.toggle('show-div');
+}
 
 for (i=0; i < button.length; i++) {
   button[i].addEventListener('mouseenter', changeColor);
-  button[i].addEventListener('mouseleave', changeColor);
-};
+  button[i].addEventListener('mouseleave', changeColor);   button[2].addEventListener('click', showDiv); button[6].addEventListener('click', showDiv);
+}
